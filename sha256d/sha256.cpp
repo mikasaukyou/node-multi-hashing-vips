b3874fc0738e696a5d9e692aee91aa81ec2b040a
ROZ <35634920+ROZ-MOFUMOFU-ME@users.noreply.github.com>
2018-08-15 05:31:05 +0900

    Update sha256.cpp

--- sha256d/sha256.cpp
+++ sha256d/sha256.cpp
@@ -8,6 +8,32 @@
 
 #include <string.h>
 
+uint32_t static inline ReadBE32(const unsigned char* ptr)
+{
+    uint32_t x;
+    memcpy((char*)&x, ptr, 4);
+    return be32toh(x);
+}
+
+uint64_t static inline ReadBE64(const unsigned char* ptr)
+{
+    uint64_t x;
+    memcpy((char*)&x, ptr, 8);
+    return be64toh(x);
+}
+
+void static inline WriteBE32(unsigned char* ptr, uint32_t x)
+{
+    uint32_t v = htobe32(x);
+    memcpy(ptr, (char*)&v, 4);
+}
+
+void static inline WriteBE64(unsigned char* ptr, uint64_t x)
+{
+    uint64_t v = htobe64(x);
+    memcpy(ptr, (char*)&v, 8);
+}
+
 // Internal implementation code.
 namespace
 {
